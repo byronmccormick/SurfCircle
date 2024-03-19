ccd1ff6f67ce8994bdce64e7fd45b93f5c152009
byron mccormick <byronjamesmccormick@gmail.com>
2024-03-19 17:52:21 +1300

    create server

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -23,7 +23,7 @@ export default function header() {
           <FaMoon />
         </Button>
         <Link to='/sign-in'>
-          <Button gradientDuoTone='purpleToBlue'>Sign In</Button>
+          <Button outline gradientDuoTone='purpleToBlue'>Sign In</Button>
         </Link>
         <NavbarToggle></NavbarToggle>
       </div>
