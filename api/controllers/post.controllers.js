a2c1614bd794b31a9f743e26810066c8e911a430
byron mccormick <byronjamesmccormick@gmail.com>
2024-05-10 12:25:51 +1200

    complete create post functionality

--- api/controllers/post.controllers.js
+++ api/controllers/post.controllers.js
@@ -4,14 +4,14 @@ import { errorHandler } from "../utils/error.js";
 export const create = async (req,res,next)=>{
 
     if(!req.user.isAdmin){
-        return next(errorHandler(403, 'You are not allowed to crfeate a post'))
+        return next(errorHandler(403, 'You are not allowed to create a post'))
     }
     if(!req.body.title || !req.body.content){
         return next(errorHandler(403, 'Please provide all required fields'))
     }
-    const slug = req.body.title.split(' '.join('-').toLowerCase().replace(/[^a-zA-Z-9-]/g,''));
+    const slug = req.body.title.split(' ').join('-').toLowerCase().replace(/[^a-zA-Z0-9-]/g, '');
     const newPost = new Post ({
-        ...req.body, slug, author: req.user._id
+        ...req.body, slug, userId: req.user.id,
     });
 
     try {
--- client/src/pages/CreatePost.jsx
+++ client/src/pages/CreatePost.jsx
@@ -6,12 +6,17 @@ import {getDownloadURL, getStorage, ref, uploadBytesResumable} from 'firebase/st
 import {app} from '../firebase'
 import { CircularProgressbar } from "react-circular-progressbar";
 import 'react-circular-progressbar/dist/styles.css';
+import {useNavigate} from 'react-router-dom'
 
 export default function CreatePost() {
     const [file, setFile] = useState(null);
     const [imageUploadProgress, setImageUploadProgress] = useState(null);
     const [imageUploadError, setImageUploadError] = useState(null);
-    const [formData, setFormData] = useState({})
+    const [formData, setFormData] = useState({});
+    const [publishError, setPublishError] = useState(null);
+
+    const navigate = useNavigate();
+
     const handleUploadImage = async () => {
         try {
             if(!file){
@@ -48,13 +53,36 @@ export default function CreatePost() {
             console.log(error)
         }
     };
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        try {
+            const res = await fetch('/api/post/create',{
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify(formData),
+            });
+            const data = await res.json();
+            if(!res.ok){
+                setPublishError(data.message)
+                return;
+            }
+            if(res.ok){
+                setPublishError(null);
+                navigate(`/post/${data.post.slug}`)
+            }
+        } catch (error) {
+            setPublishError('Something went wrong.')
+        }
+    };
   return (
     <div className="p-3 max-w-3xl mx-auto min-h-screen">
         <h1 className="text-center text-3xl my-7 font-semibold">Create a post</h1>
-        <form className="flex flex-col gap-4">
+        <form className="flex flex-col gap-4" onSubmit={handleSubmit}>
             <div className="flex flex-col gap-4 sm:flex-row justify-between">
-                <TextInput type='text' placeholder="Title" required id='title' className="flex-1"></TextInput>
-                <Select>
+                <TextInput type='text' placeholder="Title" required id='title' className="flex-1" onChange={(e)=> setFormData({...formData,title:e.target.value})}></TextInput>
+                <Select onChange={(e)=> setFormData({...formData,category:e.target.value})}>
                     <option value="uncategorized">Select a category</option>
                     <option value="php">PHP</option>
                     <option value="javascript">JavaScript</option>
@@ -77,8 +105,11 @@ export default function CreatePost() {
             </div>
             {imageUploadError && (<Alert color='failure'>{imageUploadError}</Alert>)}
             {formData.image && (<img src={formData.image} alt='upload' className='w-full h-72 object-cover'></img>)}
-            <ReactQuill theme='snow' className="h-72 mb-12" placeholder="Write something..." required></ReactQuill>
+            <ReactQuill theme='snow' className="h-72 mb-12" placeholder="Write something..." required onChange={(value)=> {setFormData({...formData,content: value})}}></ReactQuill>
             <Button type='submit' gradientDuoTone='purpleToPink'>Publish</Button>
+            {
+                publishError && <Alert className="mt-5" color='failure'>{publishError}</Alert>
+            }
         </form>
     </div>
   )
