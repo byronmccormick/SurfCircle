b44fb7a5cc87302bf5526cd5376e748e692c9fae
byron mccormick <byronjamesmccormick@gmail.com>
2024-05-14 20:08:22 +1200

    add delete user functionality to the admin user

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -40,7 +40,7 @@ export const signin = async (req,res,next)=>{
         if(!validPassword){
             return next(errorHandler(400,'Invalid password'));
         }
-        const token = jwt.sign({id: validUser._id, isAdmin: validUser.isAdmin},process.env.JWT_SECRET,{expiresIn: '5d'});
+        const token = jwt.sign({id: validUser._id, isAdmin: validUser.isAdmin},process.env.JWT_SECRET,{expiresIn: '1d'});
 
         const {password:pass,...rest}=validUser._doc
 
