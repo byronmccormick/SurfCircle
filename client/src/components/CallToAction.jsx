5b123db64f5a1e43f7b5752405c9e309439421e5
byron mccormick <byronjamesmccormick@gmail.com>
2024-06-12 22:38:44 +1200

    add content to home page

--- client/src/components/CallToAction.jsx
+++ client/src/components/CallToAction.jsx
@@ -5,15 +5,15 @@ export default function CallToAction() {
     <div className='flex flex-col sm:flex-row p-3 border border-teal-500 justify-center items-center rounded-tl-3xl rounded-br-3xl text-center'>
         <div className='flex flex-col flex-1 justify-center'>
             <h2 className="text-2xl">
-                Having a good time logging your surf sessions?
+                Looking to grow your business?
             </h2>
-            <p className="text-gray-500 my-2">Consider buying me a beer, or use this space to adverise your business!</p>
+            <p className="text-gray-500 my-2">You can support SurfCircle and reach out to hundreds of users and visitors by using this space!</p>
             <Button gradientDuoTone='purpleToPink' className="rounded-tl-xl rounded-bl-none">
-                <a href='' target='_blank'>Get in touch</a>  
+                <a href='mailto:byronjamesmccormick@gmail.com' target='_blank'>Get in touch</a>  
             </Button>
         </div>
         <div className='p-7 flex-1'>
-            <img src='https://images.unsplash.com/photo-1496449903678-68ddcb189a24?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D' alt='get your ad posted here!'></img>
+            <img src='https://www.cidewalk.com/uploads/4/9/2/1/49215843/editor/dall-e-2023-11-10-14-47-17-a-black-and-white-image-depicting-a-highway-scene-the-focus-is-on-a-large-billboard-beside-the-highway-displaying-the-message-your-ad-here-in-bold_2.png?1705074084' alt='get your ad posted here!'></img>
         </div>
     </div>
   )
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,7 +1,44 @@
-import React from 'react'
+import {useEffect, useState} from 'react';
+import {Link} from 'react-router-dom';
+import CallToAction from '../components/CallToAction' ;
+import PostCard from '../components/PostCard'
 
 export default function Home() {
+  const [posts,setPosts] = useState([])
+
+  useEffect(()=>{
+      const fetchPosts = async () => {
+        const res = await fetch('/api/post/getPosts');
+        const data = await res.json();
+        setPosts(data.posts);
+      }
+      fetchPosts();
+  }, []);
   return (
-    <div>Home</div>
+    <div className=''>
+      <div className='flex flex-col gap-6 p-28 px-3 max-w-6xl mx-auto'>
+        <h1 className='text-3xl font-bold lg:text-6xl'>Welcome to SurfCircle!</h1>
+        <p className='text-gray-500 text-xs sm:text-sm'>Join our vibrant surf community and share your stoke for waves. Whether you’re logging your surf sessions, comparing the latest forecasts, or reading about fellow surfers' adventures, SurfCircle is your go-to platform. Dive in and connect with surfers worldwide. It’s up to you how you ride the wave with us!</p>
+        <Link to='/search' className='text-xs sm:text-sm text-teal-500 font-bold hover:underline'>Check it out</Link>
+      </div>
+      <div className='p-3 bg-gray-300 dark:bg-slate-700'>
+        <CallToAction></CallToAction>
+      </div>
+      <div className='flex flex-wrap mt-5 py-7 justify-center'>
+        {
+          posts && posts.length > 0 &&(
+            <div className='flex flex-col gap-6 '>
+              <h2 className='text-2xl font-semibold text-center'>Recent Posts</h2>
+              <div className='flex flex-wrap gap-4 justify-center'>
+                {posts.map((post)=> (
+                  <PostCard key={post._id} post={post}/>
+                ))}
+              </div>
+              <Link to='/search' className='text-lg text-teal-500 text-center hover:underline'>View all posts</Link>
+            </div>
+          )
+        }
+      </div>
+    </div>
   )
 }
